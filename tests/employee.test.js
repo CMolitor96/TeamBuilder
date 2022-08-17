63c03923d1f5e1d1593a027eb31f6def7cc9c5b4
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-08-17 13:10:15 -0700

    Wrote what I believe is final logic for the constructor and all methods for employee class. Also wrote tests for constructor and all methods of employee class that all pass as of this commit

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,12 +1,45 @@
-const employee = require('../lib/employee');
+const Employee = require('../lib/employee');
 
-describe('employee', () => {
-    it('should log name to console', () => {
-        const mock = jest.spyOn(console, 'log');
-        employee('charlie');
-        mock.mockImplementation(() => { });
-        expect(mock).toBeCalledWith('charlie');
-        mock.mockRestore();
+describe('Employee', () => {
+
+    //Tests constructor function of class Employee
+    describe('Initialization', () => {
+        it('employee object should be created with name, id, and email supplied as arguments', () => {
+            const charlie = new Employee('charlie', 1, 'email@gmail');
+            expect(charlie).toEqual({name: 'charlie', id: 1, email: 'email@gmail'});
+        });
+    });
+
+    //Tests getName() function of class Employee
+    describe('getName', () => {
+        it('getName function takes in a name argument and returns the employee name', () => {
+            const foo = new Employee('foo', 1, 'email@gmail');
+            expect(foo.getName()).toEqual('foo');
+        });
+    });
+
+    //Tests getId() function of class Employee
+    describe('getId', () => {
+        it('getId function returns the Id of the specified employee', () => {
+            const bar = new Employee('bar', 1, 'email@gmail');
+            expect(bar.getId()).toEqual(1);
+        });
+    });
+
+    //Tests getEmail() function of class Employee
+    describe('getEmail', () => {
+        it('getEmail is a function that returns the email of the specified employee', () => {
+            const foobar = new Employee('Foobar', 1, 'email@email.com');
+            expect(foobar.getEmail()).toEqual('email@email.com');
+        });
+    });
+
+    //Tests getRole() function of class Employee
+    describe('getRole', () => {
+        it('getRole is a function that should return employee always for the employee class', () => {
+            const fizz = new Employee('Fizz', 1, 'email@email.com');
+            expect(fizz.getRole()).toEqual('Employee');
+        });
     });
 });
 
