dacab3b9c425313d6876adfcadeba1e1322c5118
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-08-18 10:02:04 -0700

    Added engineer subclass that extends from employee class and added tests for engineer that all pass as of now

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,12 +1,29 @@
-const engineer = require('../lib/engineer');
+const Engineer = require('../lib/engineer');
 
-describe('engineer', () => {
-    it('should console.log name of engineer', () => {
-        const mock = jest.spyOn(console, 'log');
-        engineer('jacob');
-        mock.mockImplementation(() => {});
-        expect(mock).toBeCalledWith('jacob');
-        mock.mockRestore();
+describe('Engineer', () => {
+    
+    //Tests constructor for Engineer Class
+    describe('Initialization', () => {
+        it('is a subclass of employee, and should return an object with name, id, email, and github username of new object extended from parent class', () => {
+            const foo = new Engineer('foo', 1, 'email@email', 'GitHubName');
+            expect(foo).toEqual({name:'foo', id: 1, email: 'email@email', github: 'GitHubName'});
+        });
+    });
+
+    //Tests getGithub() function for Engineer
+    describe('getGithub', () => {
+        it('should return the GitHub username of the Engineer', () => {
+            const bar = new Engineer('bar', 1, 'email@email', 'GitHubName');
+            expect(bar.getGithub()).toEqual('GitHubName');
+        });
+    });
+
+    //Tests getRole function for Engineer
+    describe('getRole', () => {
+        it('should return Engineer', () => {
+            const foobar = new Engineer('Foobar', 1, 'email@email', 'GitHubName');
+            expect(foobar.getRole()).toEqual('Engineer');
+        });
     });
 });
 
