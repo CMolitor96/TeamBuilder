cfa0ea7c65a2df7a1102fbb41ecf44890fb60d7e
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-08-18 10:10:42 -0700

    Added subclass Manager that extends Employee and added tests for Manager class that all pass as of now

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,11 +1,17 @@
-const manager = require('../lib/manager');
+const Manager = require('../lib/manager');
 
-describe('manager', () => {
-    it('should console log manager name', () => {
-        const mock = jest.spyOn(console, 'log');
-        manager('darren');
-        mock.mockImplementation(() => {});
-        expect(mock).toBeCalledWith('darren');
-        mock.mockRestore();
-    })
-})
+describe('Manager', () => {
+    describe('Initialization', () => {
+        it('Should create a new object with name, id, email, and office number', () => {
+            const foo = new Manager('Foo', 1, 'email@email', 10);
+            expect(foo).toEqual({name: 'Foo', id: 1, email: 'email@email', officeNumber: 10});
+        });
+    });
+
+    describe('getRole', () => {
+        it('Should return Manager', () => {
+            const bar = new Manager('Bar', 1, 'email@email', 10);
+            expect(bar.getRole()).toEqual('Manager');
+        });
+    });
+});
